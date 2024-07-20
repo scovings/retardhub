e324529ea7c624db8987359d65f1a1a0c7deef6b
sco. <77859647+scovings@users.noreply.github.com>
2024-07-20 04:16:13 +0100

    fixed retardhub

--- retardhub.lua
+++ retardhub.lua
@@ -1,30 +1,30 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-    Name = "RetardHub",
-    LoadingTitle = "RetardHub",
-    LoadingSubtitle = "by scovings.",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = nil, -- Create a custom folder for your hub/game
-       FileName = "RetardHub"
-    },
-    Discord = {
-       Enabled = true,
-       Invite = "https://discord.gg/cFJnKE5mbM", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = true, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "SAWEASADASDWQEQSDFFGDSFEWSDFBGGGGGGGGGGGGGGGGGDWQEQWESDASDASDASDWQSDQ",
-       Subtitle = "Key System",
-       Note = "No method of obtaining the key is provided",
-       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-    }
- })
+   Name = "Retard Hub",
+   LoadingTitle = "Retard Hub",
+   LoadingSubtitle = "by scovinq.",
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "RetardHub",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided",
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
 
 local MainTab = Window:CreateTab("Home", nil) -- Title, Image
 local MainSection = MainTab:CreateSection("Main")
@@ -47,11 +47,11 @@ Rayfield:Notify({
  local Button = MainTab:CreateButton({
     Name = "Infinite Jump",
     Callback = function()
-        loadstring(game:HttpGet('https://raw.githubusercontent.com/scovings/Aether/main/InfJump'))()
+        loadstring(game:HttpGet("https://cdn.wearedevs.net/scripts/Infinite%20Jump.txt"))()
     end,
  })
 
- local Slider = MainTab:CreateSlider({
+local Slider = MainTab:CreateSlider({
    Name = "Walkspeed",
    Range = {0, 300},
    Increment = 1,
@@ -62,4 +62,3 @@ Rayfield:Notify({
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
    end,
 })
--- sigma --
