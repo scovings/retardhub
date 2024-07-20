ffd66d092cda222035e7204cf6a936b509e68654
sco. <77859647+scovings@users.noreply.github.com>
2024-07-20 04:06:02 +0100

    Added a Key System

--- retardhub.lua
+++ retardhub.lua
@@ -14,9 +14,9 @@ local Window = Rayfield:CreateWindow({
        Invite = "https://discord.gg/cFJnKE5mbM", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
        RememberJoins = true -- Set this to false to make them join the discord every time they load it up
     },
-    KeySystem = false, -- Set this to true to use our key system
+    KeySystem = true, -- Set this to true to use our key system
     KeySettings = {
-       Title = "Untitled",
+       Title = "SAWEASADASDWQEQSDFFGDSFEWSDFBGGGGGGGGGGGGGGGGGDWQEQWESDASDASDASDWQSDQ",
        Subtitle = "Key System",
        Note = "No method of obtaining the key is provided",
        FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
@@ -62,3 +62,4 @@ Rayfield:Notify({
         game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
    end,
 })
+-- sigma --
