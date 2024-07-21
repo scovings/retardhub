abebcae72a28913eeba78df149937e50d511f4f1
sco. <77859647+scovings@users.noreply.github.com>
2024-07-21 12:01:26 +0100

    asd

--- retardhub.lua
+++ retardhub.lua
@@ -1,3 +1,9 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({Name = "Retard Library", HidePremium = false, SaveConfig = false, ConfigFolder = "RetardHub"})
+
+local Tab = Window:MakeTab({
+	Name = "TestTab",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
